e5c94b9b518154711f2b92e04143088e28881c2e
snercheg <snercheg@gmail.com>
2024-04-18 16:17:02 +0300

    added registration and isAdmin validation

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -12,12 +12,30 @@ import (
 	//TODO: push and import proto file
 )
 
-type serverApi struct {
-	ssov1.UnimplementedAuthServer
+type Auth interface {
+	Login(ctx context.Context,
+		username string,
+		email string,
+		password string,
+		appID int,
+	) (token string, err error)
+	RegisterNewUser(ctx context.Context,
+		username string,
+		email string,
+		password string,
+	) (userID int64, err error)
+	IsAdmin(ctx context.Context,
+		userID int64,
+	) (isAdmin bool, err error)
 }
 
-func Register(gRPC *grpc.Server) {
-	ssov1.RegisterAuthServer(gRPC, &serverApi{})
+type serverApi struct {
+	ssov1.UnimplementedAuthServer
+	auth Auth
+}
+
+func Register(gRPC *grpc.Server, auth Auth) {
+	ssov1.RegisterAuthServer(gRPC, &serverApi{auth: auth})
 }
 
 func (s *serverApi) Login(ctx context.Context,
@@ -25,28 +43,18 @@ func (s *serverApi) Login(ctx context.Context,
 ) (
 	*ssov1.LoginResponse, error) {
 
-	err := validation.Validate(req.GetEmail(), validation.Required, is.Email, validation.Length(3, 100))
-	if err != nil {
-		return nil, status.Error(codes.InvalidArgument, "invalid email")
-	}
-	err = validation.Validate(req.GetPassword(), validation.Required, validation.Length(5, 20))
-	if err != nil {
-		return nil, status.Error(codes.InvalidArgument, "invalid password")
-	}
-	err = validation.Validate(req.GetUsername(), validation.Required, is.Alphanumeric, validation.Length(3, 20))
-	if err != nil {
-		return nil, status.Error(codes.InvalidArgument, "invalid username")
-	}
-	err = validation.Validate(req.GetAppId(), validation.Required, is.Alphanumeric, validation.Length(1, 10))
-	if err != nil {
-		return nil, status.Error(codes.InvalidArgument, "invalid app id")
+	if err := validateLogin(req); err != nil {
+		return nil, err
 	}
 
+	token, err := s.auth.Login(ctx, req.GetUsername(), req.GetEmail(), req.GetPassword(), int(req.GetAppId()))
+	if err != nil {
+		// TODO: handle error type
+		return nil, status.Error(codes.Internal, "internal error")
+	}
 	return &ssov1.LoginResponse{
-		Token: "",
+		Token: token,
 	}, nil
-	// TODO: Generate token
-	panic("implement me")
 }
 
 func (s *serverApi) Register(
@@ -54,12 +62,78 @@ func (s *serverApi) Register(
 	req *ssov1.RegisterRequest,
 ) (
 	*ssov1.RegisterResponse, error) {
-	panic("implement me")
+	if err := validateRegister(req); err != nil {
+		return nil, err
+	}
+	userID, err := s.auth.RegisterNewUser(ctx, req.GetUsername(), req.GetEmail(), req.GetPassword())
+	if err != nil {
+		// TODO: handle error type
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+	return &ssov1.RegisterResponse{
+		UserId: userID,
+	}, nil
 }
 
 func (s *serverApi) isAdmin(
 	ctx context.Context,
 	req *ssov1.IsAdminRequest) (
 	*ssov1.IsAdminResponse, error) {
-	panic("implement me")
+
+	if err := validateIsAdmin(req); err != nil {
+		return nil, err
+	}
+
+	isAdmin, err := s.auth.IsAdmin(ctx, req.GetUserId())
+	if err != nil {
+		// TODO: handle error type
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	return &ssov1.IsAdminResponse{
+		IsAdmin: isAdmin,
+	}, nil
+}
+
+func validateLogin(req *ssov1.LoginRequest) error {
+	err := validation.Validate(req.GetEmail(), validation.Required, is.Email, validation.Length(3, 100))
+	if err != nil {
+		return status.Error(codes.InvalidArgument, "invalid email")
+	}
+	err = validation.Validate(req.GetPassword(), validation.Required, validation.Length(5, 20))
+	if err != nil {
+		return status.Error(codes.InvalidArgument, "invalid password")
+	}
+	err = validation.Validate(req.GetUsername(), validation.Required, is.Alphanumeric, validation.Length(3, 20))
+	if err != nil {
+		return status.Error(codes.InvalidArgument, "invalid username")
+	}
+	err = validation.Validate(req.GetAppId(), validation.Required, is.Alphanumeric, validation.Length(1, 10))
+	if err != nil {
+		return status.Error(codes.InvalidArgument, "invalid app id")
+	}
+	return nil
+}
+
+func validateRegister(req *ssov1.RegisterRequest) error {
+	err := validation.Validate(req.GetEmail(), validation.Required, is.Email, validation.Length(3, 100))
+	if err != nil {
+		return status.Error(codes.InvalidArgument, "invalid email")
+	}
+	err = validation.Validate(req.GetPassword(), validation.Required, validation.Length(5, 20))
+	if err != nil {
+		return status.Error(codes.InvalidArgument, "invalid password")
+	}
+	err = validation.Validate(req.GetUsername(), validation.Required, is.Alphanumeric, validation.Length(3, 20))
+	if err != nil {
+		return status.Error(codes.InvalidArgument, "invalid username")
+	}
+	return nil
+}
+
+func validateIsAdmin(req *ssov1.IsAdminRequest) error {
+	if err := validation.Validate(req.GetUserId(), validation.Required, is.Alphanumeric, validation.Length(1, 20)); err != nil {
+		return status.Error(codes.InvalidArgument, "invalid user id")
+	}
+	return nil
 }
