699b33b25945589d1a8f04592d6cad39c5d291f0
snercheg <snercheg@gmail.com>
2024-04-19 11:33:07 +0300

    added jwt gen

---
+++ internal/lib/jwt/jwt.go
@@ -0,0 +1,23 @@
+package jwt
+
+import (
+	"EMP_Back/internal/domain/models"
+	"github.com/golang-jwt/jwt/v5"
+	"time"
+)
+
+func NewToken(user models.User, app models.App, duration time.Duration) (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	claims := token.Claims.(jwt.MapClaims)
+	claims["user_id"] = user.ID
+	claims["user_email"] = user.Email
+	claims["exp"] = time.Now().Add(duration).Unix()
+	claims["app_id"] = app.ID
+
+	tokenString, err := token.SignedString([]byte(app.Secret))
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
