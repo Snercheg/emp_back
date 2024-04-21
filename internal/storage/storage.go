968ebfcfab86e0af5ff574f0f57a869ac8eccda7
snercheg <snercheg@gmail.com>
2024-04-21 16:14:31 +0300

    added more error types

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -3,7 +3,13 @@ package storage
 import "errors"
 
 var (
-	ErrUserExist    = errors.New("user exist")
-	ErrUserNotFound = errors.New("user not found")
-	ErrAppNotFound  = errors.New("app not found")
+	ErrUserExist              = errors.New("user exist")
+	ErrUserNotFound           = errors.New("user not found")
+	ErrAppNotFound            = errors.New("app not found")
+	ErrModuleNotFound         = errors.New("module not found")
+	ErrRecommendationNotFound = errors.New("recommendation not found")
+	ErrPlantFamilyNotFound    = errors.New("plant family not found")
+	ErrPlantFamilyExist       = errors.New("plant family exist")
+	ErrSettingNotFound        = errors.New("setting not found")
+	ErrDataNotFound           = errors.New("data not found")
 )
