cc88c155170397c0329c60571d29e0b020546d40
snercheg <snercheg@gmail.com>
2024-04-23 15:47:06 +0300

    added new error

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -8,6 +8,7 @@ var (
 	ErrAppNotFound            = errors.New("app not found")
 	ErrModuleNotFound         = errors.New("module not found")
 	ErrRecommendationNotFound = errors.New("recommendation not found")
+	ErrRecommendationExist    = errors.New("recommendation exist")
 	ErrPlantFamilyNotFound    = errors.New("plant family not found")
 	ErrPlantFamilyExist       = errors.New("plant family exist")
 	ErrSettingNotFound        = errors.New("setting not found")
