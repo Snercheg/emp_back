389ec4fbb5369584ed00017cacf5191998446d4c
snercheg <snercheg@gmail.com>
2024-04-23 15:46:40 +0300

    fixed recommendations table

--- internal/domain/models/recommendations.go
+++ internal/domain/models/recommendations.go
@@ -1,14 +1,16 @@
 package models
 
+import "time"
+
 type Recommendation struct {
-	ID             int     `json:"id"`
-	Title          string  `json:"title"`
-	TemperatureMin float64 `json:"temperature_min"`
-	TemperatureMax float64 `json:"temperature_max"`
-	HumidityMin    float64 `json:"humidity_min"`
-	HumidityMax    float64 `json:"humidity_max"`
-	IlluminanceMin float64 `json:"illuminance_min"`
-	IlluminanceMax float64 `json:"illuminance_max"`
-	Description    string  `json:"description"`
-	//CreatedAt      time.Duration  `json:"created_at"`
+	ID              int           `json:"id"`
+	Title           string        `json:"title"`
+	TemperatureMin  float64       `json:"temperature_min"`
+	TemperatureMax  float64       `json:"temperature_max"`
+	HumidityMin     float64       `json:"humidity_min"`
+	HumidityMax     float64       `json:"humidity_max"`
+	IlluminationMin float64       `json:"illuminance_min"`
+	IlluminationMax float64       `json:"illuminance_max"`
+	Description     string        `json:"description"`
+	CreatedAt       time.Duration `json:"created_at"`
 }
