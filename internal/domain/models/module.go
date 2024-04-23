9dee04b55538ea84f8a006458696ae0310e356ba
snercheg <snercheg@gmail.com>
2024-04-23 15:44:46 +0300

    changed module model

--- internal/domain/models/module.go
+++ internal/domain/models/module.go
@@ -1,10 +1,10 @@
 package models
 
 type module struct {
-	Id     int    `json:"id"`
-	Name   string `json:"name"`
-	ApiKey string `json:"apikey"`
-	/*	SettingId int    `json:"setting_id"`
-		TypeId    int    `json:"type_id"`*/
-	//Status string `json:"status"`
+	Id        int    `json:"id"`
+	Name      string `json:"name"`
+	ApiKey    string `json:"apikey"`
+	SettingId int    `json:"setting_id"`
+	TypeId    int    `json:"type_id"`
+	Status    string `json:"status"`
 }
