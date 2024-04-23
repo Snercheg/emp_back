d323cd244c6ae58ae1b58fb2f9ffb926cf227c73
snercheg <snercheg@gmail.com>
2024-04-23 15:47:27 +0300

    changed user model

--- internal/domain/models/user.go
+++ internal/domain/models/user.go
@@ -1,8 +1,13 @@
 package models
 
+import "time"
+
 type User struct {
-	ID       int64
-	Username string
-	Email    string
-	PassHash []byte
+	ID        int64
+	Username  string
+	Email     string
+	PassHash  []byte
+	Status    string
+	RoleId    int64
+	CreatedAt time.Duration
 }
