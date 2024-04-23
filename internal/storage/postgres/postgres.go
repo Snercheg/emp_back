43e2193343282966828a82748bd02e510716cd81
snercheg <snercheg@gmail.com>
2024-04-23 15:46:26 +0300

    added recommendation plant family layer for db

--- internal/storage/postgres/postgres.go
+++ internal/storage/postgres/postgres.go
@@ -8,6 +8,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/jackc/pgx/v5/pgconn"
+
 	"os"
 )
 
@@ -64,7 +65,7 @@ func (s *Storage) User(ctx context.Context, email string) (models.User, error) {
 	var u models.User
 	err := row.Scan(&u.ID, &u.Username, &u.Email, &u.PassHash)
 	if err != nil {
-		if err == sql.ErrNoRows {
+		if errors.Is(err, sql.ErrNoRows) {
 			return u, storage.ErrUserNotFound
 		}
 		return u, fmt.Errorf("%s: %v", op, err)
@@ -79,10 +80,109 @@ func (s *Storage) IsAdmin(ctx context.Context, id int64) (bool, error) {
 	var isAdmin bool
 	err := row.Scan(&isAdmin)
 	if err != nil {
-		if err == sql.ErrNoRows {
+		if errors.Is(err, sql.ErrNoRows) {
 			return isAdmin, storage.ErrAppNotFound
 		}
 		return isAdmin, fmt.Errorf("%s: %v", op, err)
 	}
 	return isAdmin, nil
+}
+
+// SaveRecommendation saves a new plant recommendation.
+func (s *Storage) SaveRecommendation(ctx context.Context, r *models.Recommendation) (int64, error) {
+	op := "storage.postgres.SaveRecommendation"
+	_, err := s.db.Prepare("INSERT INTO recommendations (title, temperature_min, temperature_max, humidity_min, humidity_max, illumination_min, illumination_max, description) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id")
+	if err != nil {
+		return 0, fmt.Errorf("%s: %v", op, err)
+	}
+	res, err := s.db.ExecContext(ctx, r.Title, r.TemperatureMin, r.TemperatureMax, r.HumidityMin, r.HumidityMax, r.IlluminationMin, r.IlluminationMax, r.Description)
+	if err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) && pgErr.Code == "23505" { // duplicate key
+			return 0, fmt.Errorf("%s: %v", op, err, storage.ErrRecommendationExist)
+		}
+		return 0, fmt.Errorf("%s: %v", op, err)
+	}
+	// Return the ID of the inserted row.
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("%s: %v", op, err)
+	}
+	return id, nil
+}
+
+// Recommendation returns a recommendation by ID.
+func (s *Storage) Recommendation(ctx context.Context, id int64) (models.Recommendation, error) {
+	op := "storage.postgres.Recommendation"
+	row := s.db.QueryRowContext(ctx, "SELECT id, title, temperature_min, temperature_max, humidity_min, humidity_max, illumination_min, illumination_max, description FROM recommendations WHERE id = $1", id)
+	var r models.Recommendation
+	err := row.Scan(&r.ID, &r.Title, &r.TemperatureMin, &r.TemperatureMax, &r.HumidityMin, &r.HumidityMax, &r.IlluminationMin, &r.IlluminationMax, &r.Description)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return r, storage.ErrRecommendationNotFound
+		}
+		return r, fmt.Errorf("%s: %v", op, err)
+	}
+	return r, nil
+}
+
+// SavePlantFamily saves a new plant family.
+func (s *Storage) SavePlantFamily(ctx context.Context, p *models.PlantFamily, recommendationId int64) (int64, error) {
+	op := "storage.postgres.SavePlantFamily"
+	_, err := s.db.Prepare("INSERT INTO plant_families (name, description, recommendation_id) VALUES ($1, $2, $3) RETURNING id")
+	if err != nil {
+		return 0, fmt.Errorf("%s: %v", op, err)
+	}
+	// check if recommendation exists in recommendation table.
+	row := s.db.QueryRowContext(ctx, "SELECT id FROM recommendations WHERE id = $1", recommendationId)
+
+	err = row.Scan(&recommendationId)
+	if err != nil {
+		return 0, fmt.Errorf("%s: %v", op, err)
+	}
+
+	res, err := s.db.ExecContext(ctx, p.Name, p.Description, recommendationId)
+	if err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) && pgErr.Code == "23505" { // duplicate key
+			return 0, fmt.Errorf("%s: %v", op, err, storage.ErrPlantFamilyExist)
+		}
+		return 0, fmt.Errorf("%s: %v", op, err)
+	}
+	// Return the ID of the inserted row.
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("%s: %v", op, err)
+	}
+	return id, nil
+}
+
+// PlantFamily returns a plant family by ID.
+func (s *Storage) PlantFamily(ctx context.Context, id int64) (models.PlantFamily, error) {
+	op := "storage.postgres.PlantFamily"
+	row := s.db.QueryRowContext(ctx, "SELECT id, name, description, recommendation_id FROM plant_families WHERE id = $1", id)
+	var p models.PlantFamily
+	err := row.Scan(&p.ID, &p.Name, &p.Description, &p.RecommendationId)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return p, storage.ErrPlantFamilyNotFound
+		}
+		return p, fmt.Errorf("%s: %v", op, err)
+	}
+	return p, nil
+}
+
+// PlantFamilyByName PlantFamily returns a plant family by name.
+func (s *Storage) PlantFamilyByName(ctx context.Context, name string) (models.PlantFamily, error) {
+	op := "storage.postgres.PlantFamilyByName"
+	row := s.db.QueryRowContext(ctx, "SELECT id, name, description, recommendation_id FROM plant_families WHERE name = $1", name)
+	var p models.PlantFamily
+	err := row.Scan(&p.ID, &p.Name, &p.Description, &p.RecommendationId)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return p, storage.ErrPlantFamilyNotFound
+		}
+		return p, fmt.Errorf("%s: %v", op, err)
+	}
+	return p, nil
 }
