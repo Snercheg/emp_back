e618b50d8fed0f94690b6c7b32df2d5d7f5c2481
snercheg <snercheg@gmail.com>
2024-05-02 14:26:20 +0300

    fixed link in plantFamily

--- internal/domain/models/plantFamily.go
+++ internal/domain/models/plantFamily.go
@@ -1,8 +1,9 @@
 package models
 
 type PlantFamily struct {
-	ID               int    `json:"id"`
-	Name             string `json:"name"`
-	Description      string `json:"description"`
-	RecommendationId int    `json:"recommendation_id"`
+	ID               int             `json:"id"`
+	Name             string          `json:"name"`
+	Description      string          `json:"description"`
+	RecommendationId int             `json:"recommendation_id"`
+	Recommendation   *Recommendation `json:"recommendation"`
 }
