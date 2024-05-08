e14f89d33d426f83cfec7471d33b32c9515a7712
snercheg <snercheg@gmail.com>
2024-05-08 13:08:03 +0300

    fixed user structure

--- internal/domain/models/user.go
+++ internal/domain/models/user.go
@@ -1,14 +1,11 @@
 package models
 
-import "time"
-
 type User struct {
-	ID        int64
-	Username  string
-	Email     string
-	PassHash  []byte
-	Status    string
-	RoleId    int64
-	Role      *Role
-	CreatedAt time.Duration
+	ID       int64
+	Username string
+	Email    string
+	PassHash []byte
+	Status   string
+	RoleId   int64
+	Role     *Role
 }
