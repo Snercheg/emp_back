3d2edf9620468b70432155c04336d67e2be809af
snercheg <snercheg@gmail.com>
2024-06-10 21:09:47 +0300

    fixed recommendations model

--- internal/domain/models/recommendations.go
+++ internal/domain/models/recommendations.go
@@ -5,8 +5,10 @@ type Recommendation struct {
 	Title           string  `json:"title"`
 	TemperatureMin  float64 `json:"temperature_min"`
 	TemperatureMax  float64 `json:"temperature_max"`
-	HumidityMin     float64 `json:"humidity_min"`
-	HumidityMax     float64 `json:"humidity_max"`
+	HumidityInMin   float64 `json:"humidity_in_min"`
+	HumidityInMax   float64 `json:"humidity_in_max"`
+	HumidityOutMin  float64 `json:"humidity_out_min"`
+	HumidityOutMax  float64 `json:"humidity_out_max"`
 	IlluminationMin float64 `json:"illuminance_min"`
 	IlluminationMax float64 `json:"illuminance_max"`
 	Description     string  `json:"description"`
