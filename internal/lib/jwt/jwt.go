d0e6cacec488c0360fe0e7e17f26476226972fd7
snercheg <snercheg@gmail.com>
2024-06-10 20:57:49 +0300

    added jwt auth

--- internal/lib/jwt/jwt.go
+++ internal/lib/jwt/jwt.go
@@ -2,18 +2,33 @@ package jwt
 
 import (
 	"EMP_Back/internal/domain/models"
+	"errors"
 	"github.com/golang-jwt/jwt/v5"
 	"time"
 )
 
+const (
+	signingKey = "EMP_Back_Token"
+	salt       = "EMP_Back_Token"
+)
+
+type tokenClaims struct {
+	jwt.Claims
+	UserID    int64  `json:"user_id"`
+	UserEmail string `json:"user_email"`
+	UserRole  bool   `json:"user_role"`
+}
+
 func NewToken(user models.User, duration time.Duration) (string, error) {
-	salt := "EMP_Back_Token"
 	token := jwt.New(jwt.SigningMethodHS256)
 
+	token.Claims = &tokenClaims{
+		UserID:    user.ID,
+		UserEmail: user.Email,
+		UserRole:  user.IsAdmin,
+	}
 	claims := token.Claims.(jwt.MapClaims)
-	claims["user_id"] = user.ID
-	claims["user_email"] = user.Email
-	claims["user_role"] = user.IsAdmin
+
 	claims["exp"] = time.Now().Add(duration).Unix()
 
 	tokenString, err := token.SignedString([]byte(salt))
@@ -21,4 +36,23 @@ func NewToken(user models.User, duration time.Duration) (string, error) {
 		return "", err
 	}
 	return tokenString, nil
+}
+
+func ParseToken(accessToken string) (int64, error) {
+	claims := &tokenClaims{}
+	token, err := jwt.ParseWithClaims(accessToken, claims, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("invalid signing method")
+		}
+		return []byte(signingKey), nil
+	})
+	if err != nil {
+		return 0, err
+	}
+
+	if token.Valid {
+		return claims.UserID, nil
+	}
+
+	return 0, errors.New("token is invalid")
 }
