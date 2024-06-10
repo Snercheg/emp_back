1824e683115d66ac4b991585e1669f0ef7ae07e9
snercheg <snercheg@gmail.com>
2024-06-10 20:56:35 +0300

    added settings handler

--- internal/domain/models/setting.go
+++ internal/domain/models/setting.go
@@ -1,12 +1,14 @@
 package models
 
 type Setting struct {
-	ID              int     `json:"id"`
+	ModuleId        int     `json:"module_id"`
 	Name            string  `json:"name"`
 	TemperatureMin  float64 `json:"temperature_min"`
 	TemperatureMax  float64 `json:"temperature_max"`
-	HumidityMin     float64 `json:"humidity_min"`
-	HumidityMax     float64 `json:"humidity_max"`
+	HumidityInMin   float64 `json:"humidity_in_min"`
+	HumidityInMax   float64 `json:"humidity_in_max"`
+	HumidityOutMin  float64 `json:"humidity_out_min"`
+	HumidityOutMax  float64 `json:"humidity_out_max"`
 	IlluminationMin float64 `json:"illumination_min"`
 	IlluminationMax float64 `json:"illumination_max"`
 }
