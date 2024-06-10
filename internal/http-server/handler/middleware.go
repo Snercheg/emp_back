758b3a072d46eae11b4cfe8d98ee098ccfab86ce
snercheg <snercheg@gmail.com>
2024-06-10 21:02:20 +0300

    added getUserId func

--- internal/http-server/handler/middleware.go
+++ internal/http-server/handler/middleware.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"EMP_Back/internal/lib/api/response"
+	"errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"strings"
@@ -31,4 +32,39 @@ func (h *Handler) userIdentify(c *gin.Context) {
 		return
 	}
 	c.Set(userCtxKey, userId)
+}
+
+func (h *Handler) getUserID(c *gin.Context) (int64, error) {
+	id, ok := c.Get(userCtxKey)
+	if !ok {
+		response.NewErrorResponse(c, http.StatusInternalServerError, "user id not found")
+		return 0, errors.New("user id not found")
+	}
+	idInt, ok := id.(int64)
+	if !ok {
+		response.NewErrorResponse(c, http.StatusInternalServerError, "user id is invalid")
+		return 0, errors.New("user id is not found")
+	}
+
+	return idInt, nil
+}
+
+func (h *Handler) checkIfUserIsAdmin(c *gin.Context) {
+	id, err := h.getUserID(c)
+	if err != nil {
+		response.NewErrorResponse(c, http.StatusUnauthorized, "user is not found")
+		return
+	}
+
+	isAdmin, err := h.services.Authorization.IsAdmin(id)
+	if err != nil {
+		response.NewErrorResponse(c, http.StatusInternalServerError, "error checking user status")
+		return
+	}
+
+	if !isAdmin {
+		response.NewErrorResponse(c, http.StatusUnauthorized, "user is not allowed")
+		return
+	}
+
 }
